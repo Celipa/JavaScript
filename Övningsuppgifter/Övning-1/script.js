9ddd58feecd4780adeac5b699a0e5d18a2888ea9
Celipa <Saama.alva@hotmail.com>
2023-11-08 13:33:36 +0100

    Uppgift 1

--- Övningsuppgifter/Övning-1/script.js
+++ Övningsuppgifter/Övning-1/script.js
@@ -1,112 +1,121 @@
+
+
 const quotes = [
   {
     id: 1,
-    content: 'Att ha många vänner är att inte ha några vänner.',
-    author: 'Aristoteles'
+    content: '"Att ha många vänner är att inte ha några vänner."',
+    author: ' ~ Aristoteles'
   },
   {
     id: 2,
-    content: 'Alla stora sanningar börjar som hädelser.',
-    author: 'George Bernard Shaw'
+    content: '"Alla stora sanningar börjar som hädelser."',
+    author: '~ George Bernard Shaw'
   },
   {
     id: 3,
-    content: 'Folk talar vällustigt om landet som flödar av mjölk och honung. Det skulle väl ingen vettig människa vilja gå och trampa i en sån smörja.',
-    author: 'Tage Danielsson'
+    content: '"Folk talar vällustigt om landet som flödar av mjölk och honung. Det skulle väl ingen vettig människa vilja gå och trampa i en sån smörja."',
+    author: '~ Tage Danielsson'
   },
   {
     id: 4,
-    content: 'Det vänliga ordet som sägs idag kan bära frukt i morgon.',
-    author: 'Mahatma Gandhi'
+    content: '"Det vänliga ordet som sägs idag kan bära frukt i morgon."',
+    author: '~ Mahatma Gandhi'
   },
   {
     id: 5,
-    content: 'Den som har byns vackraste fru har också den sämsta nattsömnen.',
-    author: 'bulgariskt ordspråk'
+    content: '"Den som har byns vackraste fru har också den sämsta nattsömnen."',
+    author: '~ bulgariskt ordspråk'
   },
   {
     id: 6,
-    content: 'Att ha tillräckligt är lycka, att ha mer än tillräckligt är olyckligt. Det gäller för allting, men särskilt om pengar.',
-    author: 'Lao Tse'
+    content: '"Att ha tillräckligt är lycka, att ha mer än tillräckligt är olyckligt. Det gäller för allting, men särskilt om pengar."',
+    author: '~ Lao Tse'
   },
   {
     id: 7,
-    content: 'Jag tänker, alltså finns jag.',
-    author: 'René Descartes'
+    content: '"Jag tänker, alltså finns jag."',
+    author: '~ René Descartes'
   },
   {
     id: 8,
-    content: 'Att vara materialist är att erkänna den objektiva sanningen, som blir tillgänglig för oss genom sinnesorganen.',
-    author: 'Vladimir Lenin'
+    content: '"Att vara materialist är att erkänna den objektiva sanningen, som blir tillgänglig för oss genom sinnesorganen."',
+    author: '~ Vladimir Lenin'
   },
   {
     id: 9,
-    content: 'Att vara materialist är att erkänna den objektiva sanningen, som blir tillgänglig för oss genom sinnesorganen.',
-    author: 'Vladimir Lenin'
+    content: '"Att vara materialist är att erkänna den objektiva sanningen, som blir tillgänglig för oss genom sinnesorganen."',
+    author: '~ Vladimir Lenin'
   },
   {
     id: 10,
-    content: 'De flesta författare betraktar sanningen som deras dyrbaraste ägodel; de är därför snåla med att utnyttja den',
-    author: 'Mark Twain'
+    content: '"De flesta författare betraktar sanningen som deras dyrbaraste ägodel; de är därför snåla med att utnyttja den"',
+    author: '~ Mark Twain'
   },
   {
     id: 11,
-    content: 'Att utföra är att vara.',
-    author: 'Sokrates'
+    content: '"Att utföra är att vara."',
+    author: '~ Sokrates'
   },
   {
     id: 12,
-    content: 'Vill du får nåt sagt, be en man. Vill du få nåt gjort, be en kvinna.',
-    author: 'Margaret Thatcher'
+    content: '"Vill du får nåt sagt, be en man. Vill du få nåt gjort, be en kvinna."',
+    author: '~ Margaret Thatcher'
   },
   {
     id: 13,
-    content: 'Veni, Vidi, Vici.',
-    author: 'Julius Caesar'
+    content: '"Veni, Vidi, Vici."',
+    author: '~ Julius Caesar'
   },
   {
     id: 14,
-    content: 'Att träta med en kvinna är som att försöka läsa tidning i storm.',
-    author: 'Fjodor Dostojevskij'
+    content: '"Att träta med en kvinna är som att försöka läsa tidning i storm."',
+    author: '~ Fjodor Dostojevskij'
   },
   {
     id: 15,
-    content: 'Människan är en lina, spänd mellan djuret och övermänniskan.',
-    author: 'Friedrich Nietzsche'
+    content: '"Människan är en lina, spänd mellan djuret och övermänniskan."',
+    author: '~ Friedrich Nietzsche'
   },
   {
     id: 16,
-    content: 'Det måste finnas en orgelton.',
-    author: 'Yngve Larsson'
+    content: '"Det måste finnas en orgelton."',
+    author: '~ Yngve Larsson'
   },
   {
     id: 17,
-    content: 'Demokrati är styre genom gemensam okunnighet.',
-    author: 'Elbert Hubbard'
+    content: '"Demokrati är styre genom gemensam okunnighet."',
+    author: '~ Elbert Hubbard'
   },
   {
     id: 18,
-    content: 'Skillnaden mellan geniet och dumheten är att geniet har sin begränsning.',
-    author: 'Thorvald Gahlin'
+    content: '"Skillnaden mellan geniet och dumheten är att geniet har sin begränsning."',
+    author: '~ Thorvald Gahlin'
   },
   {
     id: 19,
-    content: 'Man bör tänka sig själv som en person som finns i framtiden och kan bedöma - fördöma eller gilla - det jag som handlar idag, det jag som häller stånd eller sviker.',
-    author: 'Eyvind Johnson'
+    content: '"Man bör tänka sig själv som en person som finns i framtiden och kan bedöma - fördöma eller gilla - det jag som handlar idag, det jag som häller stånd eller sviker."',
+    author: '~ Eyvind Johnson'
   },
   {
     id: 20,
-    content: 'Fantasi är viktigare än kunskap. Kunskapen är begränsad. Fantasin omgärdar världen.',
-    author: 'Albert Einstein'
+    content: '"Fantasi är viktigare än kunskap. Kunskapen är begränsad. Fantasin omgärdar världen."',
+    author: '~ Albert Einstein'
   },
   {
     id: 21,
-    content: 'Om du tänker för länge på nästa steg, kommer du att tillbringa ditt liv stående på ett ben.',
-    author: 'kinesiskt ordspråk'
+    content: '"Om du tänker för länge på nästa steg, kommer du att tillbringa ditt liv stående på ett ben."',
+    author: '~ kinesiskt ordspråk'
   },
   {
     id: 22,
-    content: 'Det är mycket nedslående att leva i en tid då det är mycket lättare att spränga en atombomb än en fördom.',
-    author: 'Albert Einstein'
+    content: '"Det är mycket nedslående att leva i en tid då det är mycket lättare att spränga en atombomb än en fördom."',
+    author: '~ Albert Einstein'
   },
 ]
+//Väljer ett slumpmässigt citat från arrayen genom att använda 'math.random()' och 'math.floor() metoder.
+//och sen sätter 'innerHTML' delen av 'demo' elementet till 'content' av det valda citatet'
+//det kommer att ändra innehåller av de elementet till att visa ett slumpmässigt citat från arrayen'
+const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+document.getElementById("content").innerHTML = randomQuote.content;
+//sen la jag bara till en ny rad i html koden som heter author och ändrade id namnet till author här också.
+document.getElementById("author").innerHTML = randomQuote.author;
